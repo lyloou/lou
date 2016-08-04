7999e6b4a642c3f88c304227c245cb3aab7155f8
Lou6 <lyloou6@gmail.com>
2016-08-04 15:40:28 +0800

    Added: ProgressView

--- loulib/src/main/java/com/lyloou/lou/util/Udevice.java
+++ loulib/src/main/java/com/lyloou/lou/util/Udevice.java
@@ -4,6 +4,11 @@ import android.content.Context;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 
+import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
+import java.io.Reader;
+
 /**
  * 类描述：
  * 创建人： Lou
@@ -15,9 +20,62 @@ import android.net.wifi.WifiManager;
 public class Udevice {
 
     // 需要添加权限：    <uses-permission android:name="android.permission.ACCESS_WIFI_STATE" />
+    // 注意6.0以上得到的是固定值：02:00:00:00:00:00
     public static String getMacAddress(Context context) {
         WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
         WifiInfo wifiInfo = wifiManager.getConnectionInfo();
         return wifiInfo.getMacAddress();
+    }
+
+    /**
+     * 获取手机的MAC地址（6.0以上获取方式）
+     * @return
+     */
+    public static String getMacAddress(){
+        String str="";
+        String macSerial="";
+        try {
+            Process pp = Runtime.getRuntime().exec(
+                    "cat /sys/class/net/wlan0/address ");
+            InputStreamReader ir = new InputStreamReader(pp.getInputStream());
+            LineNumberReader input = new LineNumberReader(ir);
+
+            for (; null != str;) {
+                str = input.readLine();
+                if (str != null) {
+                    macSerial = str.trim();// 去空格
+                    break;
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        if ( "".equals(macSerial)) {
+            try {
+                return loadFileAsString("/sys/class/net/eth0/address")
+                        .toUpperCase().substring(0, 17);
+            } catch (Exception e) {
+                e.printStackTrace();
+
+            }
+
+        }
+        return macSerial;
+    }
+    public static String loadFileAsString(String fileName) throws Exception {
+        FileReader reader = new FileReader(fileName);
+        String text = loadReaderAsString(reader);
+        reader.close();
+        return text;
+    }
+    public static String loadReaderAsString(Reader reader) throws Exception {
+        StringBuilder builder = new StringBuilder();
+        char[] buffer = new char[4096];
+        int readLength = reader.read(buffer);
+        while (readLength >= 0) {
+            builder.append(buffer, 0, readLength);
+            readLength = reader.read(buffer);
+        }
+        return builder.toString();
     }
 }
---
+++ loulib/src/main/java/com/lyloou/lou/view/ProgressView.java
@@ -0,0 +1,77 @@
+package com.lyloou.lou.view;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.view.View;
+
+import com.lyloou.lou.R;
+
+import butterknife.ButterKnife;
+
+/**
+ * 类描述：自定义的扇形进度条
+ * 创建人： Lou
+ * 创建时间： 2016/8/4 13:41
+ * 用法：
+ * {@code
+ * <com.lyloou.lou.view.ProgressView
+ * android:id="@+id/pv_sync"
+ * android:layout_width="38dp"
+ * android:layout_height="38dp"/>
+ * }
+ * <p>
+ * {@code
+ * int currentIndex = 20;
+ * int totalCount = 100;
+ * ProgressView pvSync = ButterKnife.findById(view, R.id.pv_sync);
+ * pvSync.updateProgress(currentIndex, totalCount);
+ * }
+ */
+public class ProgressView extends View {
+    private Paint mPaint;
+    private RectF mRectF;
+
+    public ProgressView(Context context) {
+        this(context, null);
+    }
+
+    public ProgressView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public ProgressView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        mPaint.setColor(Color.GREEN);
+        mRectF = new RectF();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        int height = getHeight();
+        int weight = getWidth();
+        // 画外环
+        mRectF.set(0, 0, weight, height);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawOval(mRectF, mPaint);
+
+        // 画扇形
+        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
+        canvas.drawArc(mRectF, -90, mAngle, true, mPaint);
+    }
+
+    private int mAngle;
+
+    public void updateProgress(int currentIndex, int totalCount) {
+        mAngle = (int) (360 * (currentIndex * 1.0f / totalCount));
+        postInvalidate();
+    }
+}
