12ae0c158777b97938c163aa8eb4e574392b6f11
Lou6 <lyloou6@gmail.com>
2016-08-17 19:40:59 +0800

    Add: Uimg.getRoundedCornerBitmap

--- loulib/src/main/java/com/lyloou/lou/util/Uimg.java
+++ loulib/src/main/java/com/lyloou/lou/util/Uimg.java
@@ -3,6 +3,12 @@ package com.lyloou.lou.util;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.util.Log;
 import android.view.View;
 
@@ -81,5 +87,31 @@ public class Uimg {
             e.printStackTrace();
         }
         return bitmap;
+    }
+
+    /**
+     * 功能：获得圆角bitmap
+     * 参考资料：http://blog.sina.com.cn/s/blog_5a6f39cf0101aqsw.html
+     * @param bitmap
+     * @param roundPx
+     * @return
+     */
+    public static Bitmap getRoundedCornerBitmap(Bitmap bitmap, float roundPx) {
+        int w = bitmap.getWidth();
+        int h = bitmap.getHeight();
+        Bitmap output = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, w, h);
+        final RectF rectF = new RectF(rect);
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawRoundRect(rectF, roundPx, roundPx, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
     }
 }
