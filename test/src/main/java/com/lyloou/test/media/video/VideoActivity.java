82bae7bf7b0bb6cf8be3c2c5c3bbd4e4bbef1f4a
lilou <lyloou@qq.com>
2019-10-13 09:15:33 +0800

    update: media video

--- test/src/main/java/com/lyloou/test/media/video/VideoActivity.java
+++ test/src/main/java/com/lyloou/test/media/video/VideoActivity.java
@@ -16,10 +16,17 @@
 
 package com.lyloou.test.media.video;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.dueeeke.videocontroller.StandardVideoController;
@@ -29,6 +36,10 @@ import com.dueeeke.videoplayer.player.VideoViewConfig;
 import com.dueeeke.videoplayer.player.VideoViewManager;
 import com.gyf.immersionbar.ImmersionBar;
 import com.lyloou.test.R;
+import com.lyloou.test.common.ItemOffsetDecoration;
+import com.lyloou.test.util.Uscreen;
+
+import java.util.List;
 
 /**
  * [Home · dueeeke/DKVideoPlayer Wiki](https://github.com/dueeeke/DKVideoPlayer/wiki)
@@ -50,11 +61,27 @@ public class VideoActivity extends AppCompatActivity {
 
         initToolbar();
         initVideoView();
+        initListView();
+    }
+
+    private void initListView() {
+        RecyclerView recyclerView = findViewById(R.id.rv_list);
+        MyAdapter adapter = new MyAdapter(this, DataUtil.getVideoList());
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.addItemDecoration(new ItemOffsetDecoration(Uscreen.dp2Px(this, 4)));
+        adapter.setListener(v -> {
+            if (!(v.getTag() instanceof String)) {
+                return;
+            }
+            String url = (String) v.getTag();
+            playUrl(url);
+        });
     }
 
     private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
-        toolbar.setTitle("视频标题");
+        toolbar.setTitle("视频");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -87,9 +114,14 @@ public class VideoActivity extends AppCompatActivity {
     }
 
     private void toChangeVideo(boolean preview) {
-        videoView.release();
         int current = getCurrentPosition(preview);
-        videoView.setUrl(URLS[current]);
+        String url = URLS[current];
+        playUrl(url);
+    }
+
+    private void playUrl(String url) {
+        videoView.release();
+        videoView.setUrl(url);
         videoView.start();
     }
 
@@ -131,5 +163,56 @@ public class VideoActivity extends AppCompatActivity {
         }
     }
 
+    static class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyHolder> {
+        Context context;
+        List<VideoBean> list;
+        private View.OnClickListener listener;
+
+
+        public void setListener(View.OnClickListener listener) {
+            this.listener = listener;
+        }
+
+        public MyAdapter(Context context, List<VideoBean> list) {
+            this.context = context;
+            this.list = list;
+        }
+
+        @NonNull
+        @Override
+        public MyHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_videoprogram_list, null);
+            return new MyHolder(view);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyHolder myHolder, int i) {
+            VideoBean videoBean = list.get(i);
+            myHolder.tvTitle.setText(videoBean.getTitle());
+            myHolder.tvDesc.setText(videoBean.getUrl());
+            myHolder.itemView.setOnClickListener(v -> {
+                v.setTag(videoBean.getUrl());
+                listener.onClick(v);
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return list.size();
+        }
+
+        static class MyHolder extends RecyclerView.ViewHolder {
+            TextView tvTitle;
+            TextView tvDesc;
+            View itemView;
+
+            public MyHolder(@NonNull View itemView) {
+                super(itemView);
+                this.itemView = itemView;
+                tvDesc = itemView.findViewById(R.id.tv_desc);
+                tvTitle = itemView.findViewById(R.id.tv_title);
+            }
+        }
+    }
 
 }
