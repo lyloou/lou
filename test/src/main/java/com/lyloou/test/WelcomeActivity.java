02793cdc6ad65fc723d4afc65eea4c854335c7a8
lilou <lyloou@qq.com>
2019-11-05 18:08:49 +0800

    update welcome

--- test/src/main/java/com/lyloou/test/WelcomeActivity.java
+++ test/src/main/java/com/lyloou/test/WelcomeActivity.java
@@ -1,17 +1,13 @@
 package com.lyloou.test;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.view.KeyEvent;
-import android.view.View;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.target.GlideDrawableImageViewTarget;
 import com.lyloou.test.util.Utime;
 
 /**
@@ -27,21 +23,12 @@ public class WelcomeActivity extends Activity {
         initView();
     }
 
-    @SuppressLint("CheckResult")
     private void initView() {
         ImageView ivWelcome = findViewById(R.id.iv_welcome);
-        // http://cdn.iciba.com/news/word/big_20181103b.jpg
         ivWelcome.setOnClickListener(v -> toNext());
         Glide.with(getApplicationContext())
                 .load(String.format("http://cdn.iciba.com/web/news/longweibo/imag/%s.jpg", Utime.getDayWithFormatOne()))
-                .into(new GlideDrawableImageViewTarget(ivWelcome) {
-                    @Override
-                    protected void setResource(GlideDrawable resource) {
-                        // https://github.com/bumptech/glide/issues/275
-                        super.setResource(resource);
-                        ivWelcome.setVisibility(View.VISIBLE);
-                    }
-                });
+                .into(ivWelcome);
         handler.postDelayed(toNext, 3600);
     }
 
