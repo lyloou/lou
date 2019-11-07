c0de75b90f64a41d75fa39525d10a537ec69d885
lilou <lyloou@qq.com>
2019-11-07 15:34:44 +0800

    update test version

--- test/src/main/java/com/lyloou/test/common/Constant.java
+++ test/src/main/java/com/lyloou/test/common/Constant.java
@@ -11,6 +11,7 @@ public interface Constant {
         Laifudao("http://api.laifudao.com/"),
         Meiriyiwen("https://interface.meiriyiwen.com/article/"),
         Weather("http://t.weather.sojson.com/"),
+        YouDao("http://fanyi.youdao.com/openapi.do?keyfrom=Yanzhikai&key=2032414398&type=data&doctype=json&version=1.1&q=car/"),
         ;
         private String url;
 
